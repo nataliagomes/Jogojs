695ff8cc7f9c6d6222e9fb9b5f5eb95d825f8600
nataliagomes <naty.pinheiro95@gmail.com>
2019-07-16 16:01:53 -0300

    função alternar simbolo

---
+++ script.js
@@ -0,0 +1,30 @@
+let divs = document.querySelectorAll("div");
+
+let d1 = document.querySelector(".d1");
+let d2 = document.querySelector(".d2");
+let d3 = document.querySelector(".d3");
+let d4 = document.querySelector(".d4");
+let d5 = document.querySelector(".d5");
+let d6 = document.querySelector(".d6");
+let d7 = document.querySelector(".d7");
+let d8 = document.querySelector(".d8");
+let d9 = document.querySelector(".d9");
+
+let click = 1
+function jogar(event){
+    console.log(click);
+    if(click % 2 == 0){
+        event.target.style.backgroundImage = 'url(img/x.png)';
+        click ++;
+    }else{
+        event.target.style.backgroundImage = 'url(img/o.png)';
+        click++;
+    }
+}
+
+
+
+for(let div of divs){
+    div.onclick = jogar;
+}
+
