810887f7dac5862669b1b573459ef6c11567a8fa
Mauricio Fagner <mauriciofagner2010@hotmail.com>
2019-07-20 18:10:15 -0300

    deixar o codigo mais limpo

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let divs = document.querySelectorAll("div");
 let click = 1;
 
 function clicar(event){
-    
+
     console.log(click);    
     
     if(event.target.classList.contains('X') || (event.target.classList.contains('O'))){
@@ -35,70 +35,28 @@ for(let div of divs){
 function validarVitoria(){
 
     //validarVitoriaX
-    if(divs[0].classList.contains('X') && divs[1].classList.contains('X') && divs[2].classList.contains('X')){
+    if(divs[0].classList.contains('X') && divs[1].classList.contains('X') && divs[2].classList.contains('X') ||
+       divs[3].classList.contains('X') && divs[4].classList.contains('X') && divs[5].classList.contains('X') ||
+       divs[6].classList.contains('X') && divs[7].classList.contains('X') && divs[8].classList.contains('X') ||
+       divs[0].classList.contains('X') && divs[4].classList.contains('X') && divs[8].classList.contains('X') ||
+       divs[2].classList.contains('X') && divs[4].classList.contains('X') && divs[6].classList.contains('X') ||
+       divs[0].classList.contains('X') && divs[3].classList.contains('X') && divs[6].classList.contains('X') ||
+       divs[1].classList.contains('X') && divs[4].classList.contains('X') && divs[7].classList.contains('X') ||
+       divs[2].classList.contains('X') && divs[5].classList.contains('X') && divs[8].classList.contains('X') ){
         alert('Jogador X foi o vencedor! Parabéns');
         window.location.reload();
-    }
-    else if(divs[3].classList.contains('X') && divs[4].classList.contains('X') && divs[5].classList.contains('X')){
-        alert('Jogador X foi o vencedor! Parabéns');
-        window.location.reload();
-    }
-    else if(divs[6].classList.contains('X') && divs[7].classList.contains('X') && divs[8].classList.contains('X')){
-        alert('Jogador X foi o vencedor! Parabéns');
-        window.location.reload();
-    }
-    else if(divs[0].classList.contains('X') && divs[4].classList.contains('X') && divs[8].classList.contains('X')){
-        alert('Jogador X foi o vencedor! Parabéns');
-        window.location.reload();
-    }
-    else if(divs[2].classList.contains('X') && divs[4].classList.contains('X') && divs[6].classList.contains('X')){
-        alert('Jogador X foi o vencedor! Parabéns');
-        window.location.reload();
-    }
-    else if(divs[0].classList.contains('X') && divs[3].classList.contains('X') && divs[6].classList.contains('X')){
-        alert('Jogador X foi o vencedor! Parabéns');
-        window.location.reload();
-    }
-    else if(divs[1].classList.contains('X') && divs[4].classList.contains('X') && divs[7].classList.contains('X')){
-        alert('Jogador X foi o vencedor! Parabéns');
-        window.location.reload();
-    }
-    else if(divs[2].classList.contains('X') && divs[5].classList.contains('X') && divs[8].classList.contains('X')){
-        alert('Jogador X foi o vencedor! Parabéns');
-        window.location.reload();
-    }
+        }
 
     //validarVitoriaO
 
-    if(divs[0].classList.contains('O') && divs[1].classList.contains('O') && divs[2].classList.contains('O')){
-        alert('Jogador O foi o vencedor! Parabéns');
-        window.location.reload();
-    }
-    else if(divs[3].classList.contains('O') && divs[4].classList.contains('O') && divs[5].classList.contains('O')){
-        alert('Jogador O foi o vencedor! Parabéns');
-        window.location.reload();
-    }
-    else if(divs[6].classList.contains('O') && divs[7].classList.contains('O') && divs[8].classList.contains('O')){
-        alert('Jogador O foi o vencedor! Parabéns');
-        window.location.reload();
-    }
-    else if(divs[0].classList.contains('O') && divs[4].classList.contains('O') && divs[8].classList.contains('O')){
-        alert('Jogador O foi o vencedor! Parabéns');
-        window.location.reload();
-    }
-    else if(divs[2].classList.contains('O') && divs[4].classList.contains('O') && divs[6].classList.contains('O')){
-        alert('Jogador O foi o vencedor! Parabéns');
-        window.location.reload();
-    }
-    else if(divs[0].classList.contains('O') && divs[3].classList.contains('O') && divs[6].classList.contains('O')){
-        alert('Jogador O foi o vencedor! Parabéns');
-        window.location.reload();
-    }
-    else if(divs[1].classList.contains('O') && divs[4].classList.contains('O') && divs[7].classList.contains('O')){
-        alert('Jogador O foi o vencedor! Parabéns');
-        window.location.reload();
-    }
-    else if(divs[2].classList.contains('O') && divs[5].classList.contains('O') && divs[8].classList.contains('O')){
+    else if(divs[0].classList.contains('O') && divs[1].classList.contains('O') && divs[2].classList.contains('O') ||
+            divs[3].classList.contains('O') && divs[4].classList.contains('O') && divs[5].classList.contains('O') ||
+            divs[6].classList.contains('O') && divs[7].classList.contains('O') && divs[8].classList.contains('O') ||
+            divs[0].classList.contains('O') && divs[4].classList.contains('O') && divs[8].classList.contains('O') ||
+            divs[2].classList.contains('O') && divs[4].classList.contains('O') && divs[6].classList.contains('O') ||
+            divs[0].classList.contains('O') && divs[3].classList.contains('O') && divs[6].classList.contains('O') ||
+            divs[1].classList.contains('O') && divs[4].classList.contains('O') && divs[7].classList.contains('O') ||
+            divs[2].classList.contains('O') && divs[5].classList.contains('O') && divs[8].classList.contains('O') ){
         alert('Jogador O foi o vencedor! Parabéns');
         window.location.reload();
     }
