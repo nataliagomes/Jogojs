5e548b9b56b9a1acba03754376d3613b9528ec94
Mauricio Fagner <mauriciofagner2010@hotmail.com>
2019-07-20 15:46:42 -0300

    apagar codigos inutilizaveis

--- script.js
+++ script.js
@@ -3,28 +3,12 @@ let imgo = 'url(img/o.png)';
 
 let divs = document.querySelectorAll("div");
 
-// let a1 = document.querySelector("#a1");
-// let a2 = document.querySelector("#a2");
-// let a3 = document.querySelector("#a3");
-// let b1 = document.querySelector("#b1");
-// let b2 = document.querySelector("#b2");
-// let b3 = document.querySelector("#b3");
-// let c1 = document.querySelector("#c1");
-// let c2 = document.querySelector("#c2");
-// let c3 = document.querySelector("#c3");
-
-
-// let posicoes = {
-//     X:[],
-//     O:[]
-// }
-
-
 let click = 1;
 
 function clicar(event){
-    console.log(click);    
     
+    console.log(click);    
+
     if(event.target.classList.contains('X') || (event.target.classList.contains('O'))){
         alert('Jogava inválida. Tente novamente!');
     }
