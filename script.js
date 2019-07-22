ace9cc08df6997b712b50e695e499ab6aaa86e40
Mauricio Fagner <mauriciofagner2010@hotmail.com>
2019-07-22 10:10:06 -0300

    finalizar jogo da velha

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
+window.onload =	function() {document.getElementById('tabuleiro').style.visibility = 'hidden';
+                            document.getElementById('placar').style.visibility = 'hidden';
+                            document.getElementById('mostrador').style.visibility = 'hidden';
+                            document.getElementById('mostradorX').style.visibility = 'hidden';
+                            document.getElementById('mostradorO').style.visibility = 'hidden';
+                        }
+
+iniciarJogo = function(){
+    document.getElementById('tabuleiro').style.visibility = 'visible';
+    document.getElementById('placar').style.visibility = 'visible';
+    document.getElementById('mostrador').style.visibility = 'visible';
+    document.getElementById('mostradorX').style.visibility = 'visible';
+    document.getElementById('home').style.visibility = 'hidden';
+}
+
+let jogadorX = document.getElementById('jogadorX');
+let jogadorO = document.getElementById('jogadorO');
+let placarX = document.getElementById('placarx');
+let placarO = document.getElementById('placaro');
+let pX = 0;
+let pO = 0;
+
 let imgx = 'url(img/x.png)';
 let imgo = 'url(img/o.png)';
 
-let divs = document.querySelectorAll("div");
+let divs = document.querySelectorAll("section");
 
-let click = 1;
+// function sortearJogador(max = 3, min = 1){
+//     return Math.floor(Math.random()*(max - min))+min;
+// }
+
+let click = 2;
+
+function atualizarMostrador(){
+    if(click % 2 == 0){
+        document.getElementById('mostradorX').style.visibility = 'visible';
+        document.getElementById('mostradorO').style.visibility = 'hidden';
+        document.getElementById('pxid').style.backgroundColor = "lightgray";
+        document.getElementById('poid').style.backgroundColor = "aliceblue";
+    }else{
+        document.getElementById('mostradorX').style.visibility = 'hidden';
+        document.getElementById('mostradorO').style.visibility = 'visible';
+        document.getElementById('pxid').style.backgroundColor = "aliceblue";
+        document.getElementById('poid').style.backgroundColor = "lightgray";
+    }
+}
 
 function clicar(event){
-
-    console.log(click);    
-    
+    console.log(click);
     if(event.target.classList.contains('X') || (event.target.classList.contains('O'))){
         alert('Jogava inválida. Tente novamente!');
     }
@@ -17,14 +55,18 @@ function clicar(event){
         event.target.style.backgroundImage = imgx;
         event.target.classList.add('X')
         click++;
-        validarVitoria()
+        validarVitoria();
+        atualizarMostrador();
+        terminarJogo();
     }
     
     else{
         event.target.style.backgroundImage = imgo;
         event.target.classList.add('O')
         click++;
-        validarVitoria()
+        validarVitoria();
+        atualizarMostrador();
+        terminarJogo();
     }   
 }
 
@@ -43,8 +85,10 @@ function validarVitoria(){
        divs[0].classList.contains('X') && divs[3].classList.contains('X') && divs[6].classList.contains('X') ||
        divs[1].classList.contains('X') && divs[4].classList.contains('X') && divs[7].classList.contains('X') ||
        divs[2].classList.contains('X') && divs[5].classList.contains('X') && divs[8].classList.contains('X') ){
-        alert('Jogador X foi o vencedor! Parabéns');
-        window.location.reload();
+        alert(`${jogadorX.value} foi o vencedor da rodada!`);
+        pX++;
+        placarX.innerHTML = pX.toString();
+        limparTabuleiro();
         }
 
     //validarVitoriaO
@@ -56,10 +100,12 @@ function validarVitoria(){
             divs[0].classList.contains('O') && divs[3].classList.contains('O') && divs[6].classList.contains('O') ||
             divs[1].classList.contains('O') && divs[4].classList.contains('O') && divs[7].classList.contains('O') ||
             divs[2].classList.contains('O') && divs[5].classList.contains('O') && divs[8].classList.contains('O') ){
-        alert('Jogador O foi o vencedor! Parabéns');
-        window.location.reload();
+        alert(`${jogadorO.value} foi o vencedor da rodada!`);
+        limparTabuleiro();
+        pO++;
+        placarO.innerHTML = pO.toString();
     }else{
-        verificarVelha()
+        verificarVelha();
     }
 }
 
@@ -70,8 +116,27 @@ function verificarVelha(){
             jogadas++
             if(jogadas == divs.length){
                 alert("EMPATE! O jogo deu velha.");
-                window.location.reload();
+                limparTabuleiro();
+                // window.location.reload();
             }
         }
+    }
+}
+
+function limparTabuleiro(){
+    for(let div of divs){
+        div.classList = '';
+        div.style.backgroundImage = '';
+    }
+}
+
+function terminarJogo(){
+    if(pX == 3){
+        alert(`${jogadorX.value} ganhou a melhor de 5! Parabéns!`);
+        window.location.reload();
+    }
+    else if(pO == 3){
+        alert(`${jogadorO.value} ganhou a melhor de 5! Parabéns!`);
+        window.location.reload();
     }
 }
